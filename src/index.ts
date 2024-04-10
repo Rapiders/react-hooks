96caff6f7c8a94079597be019c437017a23d3238
d0422 <rlfehd2013@naver.com>
2024-04-10 23:01:15 +0900

    fix: export처리가 안된 문제 해결

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import useAnimation from './useAnimation';
 import useFocusAnimation from './useFocusAnimation';
 import { useDragCarouselIndex } from './useDragIndexCarousel';
 import useDragIndexCarousel from './useDragIndexCarousel';
+import { useCarousel } from './useCarousel';
 
 export {
   useInput,
@@ -10,4 +11,5 @@ export {
   useFocusAnimation,
   useDragCarouselIndex,
   useDragIndexCarousel,
+  useCarousel,
 };
