81ad05cb61ad120f7219a1f26bb32d2cfd35ae70
d0422 <rlfehd2013@naver.com>
2024-04-10 09:56:21 +0900

    feat: add useFocusAnimation

---
+++ src/useFocusAnimation.ts
@@ -0,0 +1,41 @@
+import { useCallback, useEffect, useRef } from 'react';
+import useAnimation from './useAnimation';
+
+export default function useFocusAnimation<T extends HTMLElement>(
+  onFocusClassName: string,
+  onFocusOutClassName?: string,
+  threshold?: number
+) {
+  const elementRef = useRef<T>(null);
+  const { animationClassName, triggerUnmountAnimation } = useAnimation(
+    onFocusClassName,
+    onFocusOutClassName
+  );
+
+  const handleScroll: IntersectionObserverCallback = useCallback(([entry]) => {
+    const { current } = elementRef;
+    if (current) {
+      if (entry.isIntersecting) {
+        if (animationClassName)
+          return current.classList.add(animationClassName);
+        triggerUnmountAnimation();
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    let observer: IntersectionObserver;
+    const { current } = elementRef;
+
+    if (current) {
+      observer = new IntersectionObserver(handleScroll, {
+        threshold: threshold || 0.1,
+      });
+      observer.observe(current);
+
+      return () => observer && observer.disconnect();
+    }
+  }, [handleScroll]);
+
+  return elementRef;
+}
