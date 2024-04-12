63d226bc16df702594d19efaf9491ccd15b8a35a
d0422 <rlfehd2013@naver.com>
2024-04-12 15:46:52 +0900

    config: jest, testing-library 환경 구성

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import { compilerOptions } from './tsconfig.json';
+import { pathsToModuleNameMapper } from 'ts-jest';
+
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  preset: 'ts-jest',
+  rootDir: '.',
+  testMatch: ['**/?(*)+(test).ts'],
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  resetMocks: true,
+  // 💡 tsconfig 에서 `baseUrl` 과 `paths` 를 사용하는 경우 필요
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>',
+  }),
+};
