53544998ffc017ba73517e56f0275e72f13a744f
d0422 <rlfehd2013@naver.com>
2024-06-07 19:39:24 +0900

    test: overlayClose test 추가

--- src/useModal/useModal.test.tsx
+++ src/useModal/useModal.test.tsx
@@ -28,6 +28,72 @@ describe('useModal 기능 테스트', () => {
     expect(await screen.findByRole('test')).toBeTruthy();
   });
 
+  it('overlayClose가 true인 경우, overlay를 눌러 modal을 닫을 수 있다.', async () => {
+    const Test = () => {
+      const { Modal, show, hide } = useModal();
+      return (
+        <div>
+          <button role="show" onClick={show}>
+            SHOW
+          </button>
+          <button role="hide" onClick={hide}>
+            SHOW
+          </button>
+          <Modal>
+            <div role="test"></div>
+            <button role="modal-hide" onClick={hide}>
+              SHOW
+            </button>
+          </Modal>
+        </div>
+      );
+    };
+    render(<Test />);
+    fireEvent.click(await screen.findByRole('show'));
+    expect(await screen.findByRole('test')).toBeTruthy();
+    fireEvent.click(await screen.findByRole('hide'));
+    await expect(screen.findByRole('test')).rejects.toThrow();
+
+    fireEvent.click(await screen.findByRole('show'));
+    expect(await screen.findByRole('test')).toBeTruthy();
+    fireEvent.click(await screen.findByRole('modal-hide'));
+    await expect(screen.findByRole('test')).rejects.toThrow();
+
+    fireEvent.click(await screen.findByRole('show'));
+    const modalInner = await screen.findByRole('test');
+    fireEvent.click(modalInner!.parentElement!.parentElement!);
+    await expect(screen.findByRole('test')).rejects.toThrow();
+  });
+
+  it('overlayClose가 false인 경우, overlay를 누르면 modal이 닫히지 않는다.', async () => {
+    const Test = () => {
+      const { Modal, show, hide } = useModal({ overlayClose: false });
+      return (
+        <div>
+          <button role="show" onClick={show}>
+            SHOW
+          </button>
+          <button role="hide" onClick={hide}>
+            SHOW
+          </button>
+          <Modal>
+            <div role="test"></div>
+            <button role="modal-hide" onClick={hide}>
+              SHOW
+            </button>
+          </Modal>
+        </div>
+      );
+    };
+    render(<Test />);
+
+    fireEvent.click(await screen.findByRole('show'));
+    expect(await screen.findByRole('test')).toBeTruthy();
+    const modalInner = await screen.findByRole('test');
+    fireEvent.click(modalInner!.parentElement!.parentElement!);
+    await expect(screen.findByRole('test')).toBeTruthy();
+  });
+
   it('지정한 element에 modal을 렌더링할 수 있다.', async () => {
     const testRoot = document.createElement('div');
     testRoot.id = 'test-root';
