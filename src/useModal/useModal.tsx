c5f3798a8580bafef831737e11e33a670438f13d
d0422 <rlfehd2013@naver.com>
2024-06-07 19:07:49 +0900

    feat: 애니메이션을 지원하는useModal 기능 추가

---
+++ src/useModal/useModal.tsx
@@ -0,0 +1,63 @@
+import React, { CSSProperties, ReactNode } from 'react';
+import { createPortal } from 'react-dom';
+import { useAnimation } from '..';
+type ModalRoot = Element | DocumentFragment;
+
+export type UseModalAnimations = {
+  overlayAnimation?: {
+    showClassName?: string;
+    hideClassName?: string;
+  };
+  modalAnimation?: {
+    showClassName?: string;
+    hideClassName?: string;
+  };
+};
+
+interface UseModalProps extends UseModalAnimations {
+  modalRoot?: ModalRoot;
+}
+
+interface ModalProps {
+  overlayClassName?: string;
+  modalClassName?: string;
+  style?: CSSProperties;
+  children: ReactNode;
+}
+
+const DefaultModal = ({ children, modalRoot }: { children: ReactNode; modalRoot?: ModalRoot }) => {
+  return createPortal(children, modalRoot || document.body);
+};
+
+export default function useModal(modalProps?: UseModalProps) {
+  const ModalAnimation = useAnimation({
+    mountClassName: modalProps?.modalAnimation?.showClassName,
+    unmountClassName: modalProps?.modalAnimation?.hideClassName,
+  });
+  const OverlayAnimation = useAnimation({
+    mountClassName: modalProps?.overlayAnimation?.showClassName,
+    unmountClassName: modalProps?.overlayAnimation?.hideClassName,
+  });
+
+  const show = () => {
+    OverlayAnimation.show();
+    ModalAnimation.show();
+  };
+
+  const hide = () => {
+    OverlayAnimation.hide();
+    ModalAnimation.hide();
+  };
+
+  const Modal = ({ children, overlayClassName, modalClassName, style }: ModalProps) => (
+    <DefaultModal modalRoot={modalProps?.modalRoot}>
+      <OverlayAnimation.AnimationWrapper className={overlayClassName} onClick={hide}>
+        <ModalAnimation.AnimationWrapper className={modalClassName} style={style} onClick={(e) => e.stopPropagation()}>
+          {children}
+        </ModalAnimation.AnimationWrapper>
+      </OverlayAnimation.AnimationWrapper>
+    </DefaultModal>
+  );
+
+  return { Modal, show, hide, isShow: OverlayAnimation.isShow };
+}
